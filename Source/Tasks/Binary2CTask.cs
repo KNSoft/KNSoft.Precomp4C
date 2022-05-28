8f0e43d2f19593b04fb63b1f565ec61026ce29f1
Ratin <ratin@knsoft.org>
2022-05-28 18:20:39 +0800

    Refactor

---
+++ Source/Tasks/Binary2CTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+
+using KNSoft.C4Lib;
+
+namespace KNSoft.Precomp4C
+{
+    public class Binary2CTask : Precomp4CTask
+    {
+        [Required]
+        public required String OutputHeader { get; set; }
+
+        [Required]
+        public required String OutputSource { get; set; }
+
+        public override Boolean Execute()
+        {
+            try
+            {
+                CreateCSourceOutputStreams(OutputHeader, OutputSource, out var HeaderStream, out var SourceStream);
+
+                Byte[] Data = File.ReadAllBytes(Source);
+                UInt32 RemainSize = (UInt32)Data.Length;
+                String SymbolDef = String.Format("unsigned char {0}[{1:D}]", "Precomp4C_Binary2C_" + Path.GetFileNameWithoutExtension(Source), RemainSize);
+                Rtl.WriteToStream(HeaderStream, Encoding.UTF8.GetBytes("extern " + SymbolDef + ";\r\n"));
+                Rtl.WriteToStream(SourceStream, Encoding.UTF8.GetBytes(SymbolDef + " = {\r\n"));
+                while (RemainSize > 0)
+                {
+                    UInt32 LineSize = Math.Min(RemainSize, BytesPerLine);
+                    String Line = "    ";
+
+                    for (UInt32 j = 0; j < LineSize; j++)
+                    {
+                        Line += String.Format("0x{0:X2}", Data[(UInt32)Data.Length - RemainSize--]);
+                        if (RemainSize > 0)
+                        {
+                            Line += ", ";
+                        }
+                    }
+                    Rtl.WriteToStream(SourceStream, Encoding.UTF8.GetBytes(Line + "\r\n"));
+                }
+                Rtl.WriteToStream(SourceStream, "};\r\n"u8.ToArray());
+
+                HeaderStream.Dispose();
+                SourceStream.Dispose();
+
+                Log.LogMessage(MessageImportance.High, "\t-> " + OutputHeader);
+                Log.LogMessage(MessageImportance.High, "\t-> " + OutputSource);
+                return true;
+            } catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex, true, true, Source);
+                return false;
+            }
+        }
+    }
+}
---
+++ Source/Tasks/DllStubTask.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+using System.Linq;
+using Microsoft.Build.Framework;
+
+using KNSoft.C4Lib;
+using KNSoft.C4Lib.PEImage;
+
+namespace KNSoft.Precomp4C
+{
+    public class DllStubTask : Precomp4CTask
+    {
+        [Required]
+        public required String OutputFile { get; set; }
+
+        [Required]
+        public required String Platform { get; set; }
+
+        public override Boolean Execute()
+        {
+            try
+            {
+                List<KeyValuePair<String, List<DllStub.DllExport>>> Exports = new();
+                IMAGE_FILE_MACHINE Machine = FileHeader.GetMachineType(Platform);
+                ParseXML(Machine, Source, Exports);
+                FileStream Output = File.Open(OutputFile, FileMode.Create, FileAccess.Write);
+                Rtl.WriteToStream(Output, DllStub.MakeImportStubLibraryFile(Machine, Exports));
+                Output.Dispose();
+                Log.LogMessage(MessageImportance.High, "\t-> " + OutputFile);
+                return true;
+            } catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex, true, true, Source);
+                return false;
+            }
+        }
+
+        private static void ParseXML(IMAGE_FILE_MACHINE Machine, String XmlPath, List<KeyValuePair<String, List<DllStub.DllExport>>> Exports)
+        {
+            XmlDocument doc = new();
+            doc.Load(XmlPath);
+
+            if (doc.DocumentElement == null)
+            {
+                throw new InvalidDataException("Invalid XML file: " + XmlPath);
+            }
+
+            if (doc.DocumentElement.Name != "DllStub")
+            {
+                throw new InvalidDataException("Invalid root element: " + doc.DocumentElement.Name);
+            }
+
+            foreach (XmlElement Dll in doc.DocumentElement.GetElementsByTagName("Dll").OfType<XmlElement>())
+            {
+                XmlAttributeCollection DllAttr = Dll.Attributes;
+                String? DllName = (DllAttr["Name"]?.Value) ?? throw new ArgumentException("Dll 'Name' unspecified in: " + Dll.OuterXml);
+
+                List<DllStub.DllExport> DllExportList = new();
+                foreach (XmlElement DllExport in Dll.ChildNodes.OfType<XmlElement>())
+                {
+                    if (DllExport.Name != "Export")
+                    {
+                        throw new ArgumentException("Unrecognized element: " + DllExport.OuterXml);
+                    }
+
+                    XmlAttributeCollection ExportAttr = DllExport.Attributes;
+                    String? ExportName = (ExportAttr["Name"]?.Value) ?? throw new ArgumentException("Export 'Name' unspecified in: " + DllExport.OuterXml);
+
+                    String? CallConv, Archs, Arg, Type;
+                    String DecoratedName;
+                    IMPORT_OBJECT_NAME_TYPE NameType;
+                    IMPORT_OBJECT_TYPE ObjectType;
+
+                    /* Arch */
+                    Archs = ExportAttr["Arch"]?.Value;
+                    if (Archs != null && !Array.Exists(Archs.Split(' '), x => FileHeader.GetMachineType(x) == Machine))
+                    {
+                        continue;
+                    }
+
+                    /* CallConv */
+                    if (Machine == IMAGE_FILE_MACHINE.AMD64)
+                    {
+                        CallConv = "__fastcall";
+                    } else
+                    {
+                        CallConv = ExportAttr["CallConv"]?.Value;
+                    }
+                    if (CallConv == "__stdcall")
+                    {
+                        Arg = ExportAttr["Arg"]?.Value;
+
+                        UInt32 ArgSize = 0;
+                        if (Arg != null)
+                        {
+                            foreach (String Param in Arg.Split(' '))
+                            {
+                                ArgSize += Param switch
+                                {
+                                    "ptr" => FileHeader.GetMachineBits(Machine) / 8,
+                                    "long" => 4,
+                                    "int" => 4,
+                                    "float" => 4,
+                                    "double" => 8,
+                                    _ => throw new ArgumentException("Unrecognized argument type: " + Arg + "in " + DllExport.OuterXml)
+                                };
+                            }
+                        }
+
+                        DecoratedName = '_' + ExportName + '@' + ArgSize.ToString();
+                        NameType = IMPORT_OBJECT_NAME_TYPE.NAME_UNDECORATE;
+                    } else
+                    {
+                        DecoratedName = '_' + ExportName;
+                        NameType = IMPORT_OBJECT_NAME_TYPE.NAME;
+                    }
+
+                    /* Type */
+                    Type = ExportAttr["Type"]?.Value;
+                    if (Type != null)
+                    {
+                        ObjectType = Type switch
+                        {
+                            "Data" => IMPORT_OBJECT_TYPE.DATA,
+                            "Const" => IMPORT_OBJECT_TYPE.CONST,
+                            "Code" => IMPORT_OBJECT_TYPE.CODE,
+                            _ => throw new ArgumentException("Unrecognized 'Type' of: " + DllExport.OuterXml)
+                        };
+                    } else
+                    {
+                        ObjectType = IMPORT_OBJECT_TYPE.CODE;
+                    }
+
+                    DllExportList.Add(new(ObjectType, DecoratedName, NameType));
+                }
+                if (DllExportList.Count > 0)
+                {
+                    Exports.Add(new(DllName, DllExportList));
+                }
+            }
+        }
+    }
+}
